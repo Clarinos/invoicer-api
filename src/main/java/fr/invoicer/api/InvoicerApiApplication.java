3d329731c19a2acba26a5caa9d50f749e9d35f08
Clarence Edoh-Dagnon <clarenceedohdagnon@gmail.com>
2026-09-03 02:46:07 +0200

    chore: initial project setup with spring boot and docker postgres

---
+++ src/main/java/fr/invoicer/api/InvoicerApiApplication.java
@@ -0,0 +1,13 @@
+package fr.invoicer.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoicerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoicerApiApplication.class, args);
+	}
+
+}
